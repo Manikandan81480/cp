ab2fa6fb15d03c5c377d618e55ade6b20bd45c42
Manikandan81480 <mani81480@gmail.com>
2016-10-16 06:02:41 -0700

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,22 @@
+class Fact
+{
+public static void main(String... args)
+{
+Scanner x=new Scanner(System.in);
+int n=x.nextInt();
+int fact=1,a;
+if(n!=0)
+{
+for(int i=n;i<=1;i--)
+{
+a=i*fact;
+fact=a;
+}
+else
+{
+System.out.println(1);
+}
+}
+System.out.println(a);
+}
+}
